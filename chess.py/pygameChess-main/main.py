4f48315b0e32e9775d29202459dfb27e752edb41
not ∞ <110843279+RUPAKparajuli07@users.noreply.github.com>
2025-07-08 10:12:07 +0545

    Update main.py

--- chess.py/pygameChess-main/main.py
+++ chess.py/pygameChess-main/main.py
@@ -1,16 +1,4 @@
-import random 
-import pygame
 
-pygame.init()
-WIDTH = 1000
-HEIGHT = 900
-screen = pygame.display.set_mode([WIDTH, HEIGHT])
-pygame.display.set_caption('Two-Player Pygame Chess!')
-font = pygame.font.Font('freesansbold.ttf', 20)
-medium_font = pygame.font.Font('freesansbold.ttf', 40)
-big_font = pygame.font.Font('freesansbold.ttf', 50)
-timer = pygame.time.Clock()
-fps = 60
 # game variables and images
 white_pieces = ['rook', 'knight', 'bishop', 'king', 'queen', 'bishop', 'knight', 'rook',
                 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn']
